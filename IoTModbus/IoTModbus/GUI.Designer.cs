747db0233e43ab21f58ab9f4e7d07126b8ffaab1
AlexZG <alexandergjerseth@gmail.com>
2017-02-28 10:48:29 +0100

    Created the clases for the project

--- IoTModbus/IoTModbus/GUI.Designer.cs
+++ IoTModbus/IoTModbus/GUI.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(385, 379);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "GUI";
+            this.Text = "Modbus Client For IoT";
             this.ResumeLayout(false);
 
         }
